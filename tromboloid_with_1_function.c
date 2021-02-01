96c31a8ff606ee14f6cbc065101d2cdffae09faf
Neha cn__ENG19CS0203 <75070994+nehacn@users.noreply.github.com>
2021-02-01 22:18:29 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 int main()
 {
-   float h,d,b,V;
-   printf("Enter height,depth,breadth:");
-   scanf("%f%f%f",&h,&d,&b);
-   V=((h*d*b)+(d/b))/3;
-   printf("Volume of Tromboloid is :%f",V);
-   return 0;
+    float h,d,b,V;
+    printf("Enter height,depth,breadth:");
+    scanf("%f%f%f",&h,&d,&b);
+    V=((h*d*b)+(d/b))/3;
+    printf("Volume of Tromboloid is :%f",V);
+    return 0;
 }
