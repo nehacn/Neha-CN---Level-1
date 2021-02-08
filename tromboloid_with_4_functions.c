f68d07fa322e28dc3abb2e5b3118b60090ed14af
Neha cn__ENG19CS0203 <75070994+nehacn@users.noreply.github.com>
2021-02-08 09:40:19 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -3,29 +3,30 @@
 float input()
 {
   float height;
-  printf("\n Enter a value of height,depth,breadth ");
+  printf("Enter a Value of height,depth,breadth");
   scanf("%f",&height);
   return height;
 }
-  float find_volume(float height,float depth , float breadth)
+ float find_volume(float height,float depth,float breadth)
 {
-  float volume;
-  volume=((height*depth*breadth)+(depth/breadth))/3;
-  return volume;
+ float volume;
+ volume=((height*depth*breadth)+(depth/breadth))/3;
+ return volume;
 }
- void output(float height,float depth,float breadth,float volume)
+ void Result(float height,float depth,float breadth,float volume)
 {
-  printf(" Volume of the Tromboloid with values %f,%f,%f is %f",height,depth,breadth,volume);
+ printf("Volume of Tromboloid of values %f,%f,%f is %f",height,depth,breadth,volume);
 }
-   float main()
-{
-  float a,b,c,x;
-  a=input();
-  b=input();
-  c=input();
-  x=find_volume(a,b,c);
+ float main()
+ {
+   float a,b,c,X;
+   a=input();
+   b=input();
+   c=input();
+X=find_volume(a,b,c);
+ 
+Result(a,b,c,X);
+return 0;
 
- output(a,b,c,x);
- return 0;
 }
 
