80d58676f77a146b61c782913dabc678461584c1
Neha cn__ENG19CS0203 <75070994+nehacn@users.noreply.github.com>
2021-02-15 22:10:42 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,36 +1,56 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include<math.h>
-
-int input()
+int input1()
 {
   int X;
+  printf("\n Enter the co-ordinates of point A = ");
+  printf("\n Enter point x1:");
+  scanf("%d",&X);
+  return X;
+}
+int input2()
+{
+  int X;
+  printf("\nEnter point y1:");
+  scanf("%d",&X);
+  return X;
+}
+int input3()
+{
+  int X;
+  printf("\n Enter the co-ordinates of point B = ");
+  printf("\n Enter point x2 :");
+  scanf("%d",&X);
+  return X;
+}
+int input4()
+{
+  int X;
+  printf("\nEnter point y2:");
   scanf("%d",&X);
   return X;
 }
   int compute( int x1,int y1, int x2 , int y2)
-{ 
+{
   float Distance;
   Distance=(float)sqrt((pow((x2-x1),2)+pow((y2-y1),2)));
   printf("%f",Distance);
   return Distance;
 }
 void out( float Distance)
-{ printf("%f is the distance",Distance);
+{ printf("%f is the distance between points A and B ",Distance);
 }
  int main( )
- { 
-	int a,b,c,d;
+ {
+int a,b,c,d;
     float Distance;
-    printf("Enter point x1:");
-    a=input();
-    printf("Enter point y1:");
-    b=input();
-    printf("Enter point x2:");
-    c=input();
-    printf("Enter point y2:");
-    d=input();
+    a=input1();
+    b=input2();
+    c=input3();
+    d=input4();
     compute(a,b,c,d);
     out(Distance);
     return 0;
  }
+
