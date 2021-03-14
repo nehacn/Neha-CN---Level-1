6d101fa00c40491ed53afd23b9f80b8a13bb71ba
Neha cn__ENG19CS0203 <75070994+nehacn@users.noreply.github.com>
2021-03-14 22:48:56 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -2,39 +2,39 @@
 #include<stdio.h>
 int input()
 {
-  int X;
-  printf("\n Enter the number of elements  =");
-  scanf("%d",&X);
-  return X;
+    int X;
+    printf("Enter the no of elements = ");
+    scanf("%d",&X);
+    return X;
 }
- void input_arr(int X , int a[X])
+void inp_arr(int X,int a[X])
 {
-  printf("\n Enter the elements = ");
-  for (int i=0;i<X;i++)
-   {
-      scanf("%d",&a[i]);
-   }
+    printf("Enter the elements = ");
+    for(int i=0;i<X;i++)
+    {
+        scanf("%d",&a[i]);
+    }
 }
-int add(int X, int a[ ])
+int add(int X,int a[])
 {
-  int sum=0;
-  for(int i=0;i<X;i++)
-  {
-     sum=sum+a[i];
-  }
-  return sum;
+    int sum=0;
+    for(int i=0;i<X;i++)
+    {
+        sum=sum+a[i];
+    }
+    return sum;
 }
- void Display(int X , int sum)
+void display(int X,int sum)
 {
-  printf("\n Sum of %d different numbers is = %d",X,sum);
+    printf("Sum of %d different numbers is  d",X,sum);
 }
 int main()
 {
-  int X,sum;
-  X=input();
-  int a[X];
-  input_arr(X,a);
-  sum=add(X,a);
-  Display(X,sum);
-  return 0;
+    int X,sum;
+    X=input();
+    int a[X];
+    inp_arr(X,a);
+    sum=add(X,a);
+    display(X,sum);
+    return 0;
 }
