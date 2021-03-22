415867e82bc3dd8c2de2a68418b87fd9bea74dcf
Neha cn__ENG19CS0203 <75070994+nehacn@users.noreply.github.com>
2021-03-22 21:40:14 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,56 +1,37 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include<math.h>
-int input1()
+int input()
 {
-  int X;
-  printf("\n Enter the co-ordinates of point A = ");
-  printf("\n Enter point x1:");
-  scanf("%d",&X);
-  return X;
+    float X;
+    scanf("%f",&X);
+    return X;
 }
-int input2()
+float calculate_distance(float x1,float y1,float x2,float y2)
 {
-  int X;
-  printf("\nEnter point y1:");
-  scanf("%d",&X);
-  return X;
+    float distance;
+    distance=sqrt((y2-y1)*(y2-y1)+(x2-x1)*(x2-x1));
+    return distance;
 }
-int input3()
+float display(float distance)
 {
-  int X;
-  printf("\n Enter the co-ordinates of point B = ");
-  printf("\n Enter point x2 :");
-  scanf("%d",&X);
-  return X;
+    printf("The distance between point A and B is = %f",distance);
+    return distance;
 }
-int input4()
+int main()
 {
-  int X;
-  printf("\nEnter point y2:");
-  scanf("%d",&X);
-  return X;
-}
-  int compute( int x1,int y1, int x2 , int y2)
-{
-  float Distance;
-  Distance=(float)sqrt((pow((x2-x1),2)+pow((y2-y1),2)));
-  printf("%f",Distance);
-  return Distance;
-}
-void out( float Distance)
-{ printf("%f is the distance between points A and B ",Distance);
-}
- int main( )
- {
-int a,b,c,d;
-    float Distance;
-    a=input1();
-    b=input2();
-    c=input3();
-    d=input4();
-    compute(a,b,c,d);
-    out(Distance);
+    float x1,y1,x2,y2,distance;
+    printf("\n Enter the co-ordinates of point A =");
+    printf("\n Enter the value of x1 :");
+    x1=input();
+    printf("\n Enter the value of y1 :");
+    y1=input();
+    printf("\n Enter the co-ordinates of point B =");
+    printf("\n Enter the value of x2 :");
+    x2=input();
+    printf("\n Enter the value of y2 :");
+    y2=input();
+    distance=calculate_distance(x1,y1,x2,y2);
+    display(distance);
     return 0;
- }
-
+}
